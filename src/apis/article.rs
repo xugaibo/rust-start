b43a3f4e84bf0ceefb4d32cd79d407a95adc86d9
汤观石生(tody.tang) <tody.tang@huolala.cn>
2022-08-23 16:39:16 +0800

    修改变量名

--- src/apis/article.rs
+++ src/apis/article.rs
@@ -5,6 +5,6 @@ use crate::models::response::response::Response;
 use crate::service::article;
 
 pub async fn create(req: web::Json<CreateArticle>, data: web::Data<Context>) -> impl Responder {
-    let r = article::create(req.0, data).await;
-    return Response::from_result(&r).to_json_resp();
+    let result = article::create(req.0, data).await;
+    return Response::from_result(&result).to_json_resp();
 }
